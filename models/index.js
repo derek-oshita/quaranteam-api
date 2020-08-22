41b7b2cba1523adf2b62989e47005fb796098cac
Derek Oshita <65000424+zetsubou-kira@users.noreply.github.com>
2020-08-21 17:03:25 -0700

    Server running, models setup.

--- models/index.js
+++ models/index.js
@@ -1,6 +1,23 @@
 // IMPORTS
 const mongoose = require('mongoose'); 
 
+// TROUBLESHOOTING LOG
 console.log('MONGO DB = ', process.env.MONGODB_URI); 
 
+// MONGODB CONNECTION VARIABLES
 const connectionString = process.env.MONGODB_URI; 
+const configOptions = {
+    useNewUrlParser: true, 
+    useCreateIndex: true, 
+    useUnifiedTopology: true, 
+    useFindAndModify: false, 
+}; 
+
+mongoose.connect(connectionString, configOptions)
+    .then(() => console.log('MONGODB successfully connected...'))
+    .catch((err) => console.log(`MONGODB ERROR: ${err}`)); 
+
+module.exports = {
+    User: require('./User'), 
+    Comment: require('./Comment')
+}; 
